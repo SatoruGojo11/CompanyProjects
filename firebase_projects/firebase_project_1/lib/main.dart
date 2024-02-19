a5a21798135e5668f322c99906d7a10bc4576894
Satoru Gojo <132731923+SatoruGojo11@users.noreply.github.com>
2024-02-19 23:40:45 +0530

    Update main.dart

--- firebase_projects/firebase_project_1/lib/main.dart
+++ firebase_projects/firebase_project_1/lib/main.dart
@@ -6,14 +6,7 @@ Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // In this Project I manually Add Firebase with my Flutter App
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyD0Pje2uEUpYMTPy1gLNMdf0Qv54HrYAn4",
-      appId: "1:450937056048:android:651bf228b74fe3dec5f6f5",
-      messagingSenderId: "450937056048",
-      projectId: "jaydip-firebase-project-1",
-    ),
-  );
+  // The Code that contains key of firebase is removed 
 
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
